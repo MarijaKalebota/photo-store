5deaeaadad5cca49199c5912fa232459165b827c
Marija Kalebota Kodzoman <marija.kalebota@gmail.com>
2019-09-30 08:58:55 -0700

    Implement first test

--- photo_store/photo_store/tests.py
+++ photo_store/photo_store/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestPhotos(TestCase):
+    def test_photos_status_code_200(self):
+        response = self.client.get('http://127.0.0.1:8000/photo-store/photos/')
+        self.assertEquals(response.status_code, 200)
